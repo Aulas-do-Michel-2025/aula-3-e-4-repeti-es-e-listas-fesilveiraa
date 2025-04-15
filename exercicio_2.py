ab53fc1926d737c735310b3511a00df5de0ef2ad
fesilveiraa <fesilveira23@gmail.com>
2025-04-15 10:45:59 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,25 +1,9 @@
-"""
-#### Exercício 2 - Filtrando uma lista.
+lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))] 
+ 
+nova_lista = []
 
-Receba uma lista de input do usuário.
+for numero in lista:
+    if numero % 2 != 0:
+        nova_lista.append (numero)
 
-Ele digitará como um texto com os números separados por vígula. Para isso, pode-se utilizar o código disponibilizado que
-vai transformar esse texto em lista para você.
-
-Depois imprima uma lista apenas com os números ímpares.
-
-Dica: Crie outra lista e popule ela, a partir da varredura da lista original.
-
-Exemplos:
-
-----------------------------------
-
-Digite a sua lista (separando os números por vírgula): 1, 2, 3, 4, 5
-Resposta:
-Os números ímpares são [1, 3, 5]
-"""
-
-# Código para pegar a lista
-lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
-
-# Fazer a partir daqui...
+print (f"Os números ímpares são: {nova_lista} ") 
